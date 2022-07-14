011f8a5f14b0e30b38e66bd3f15ef750b2baac94
AlanKeeps <alan.keeps.walking@gmail.com>
2022-07-14 15:34:23 +0300

    add list item and vpAdapter

--- app/src/main/java/com/example/weather/fragment/MainFragment.kt
+++ app/src/main/java/com/example/weather/fragment/MainFragment.kt
@@ -1,5 +1,4 @@
 package com.example.weather.fragment
-
 import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,15 +7,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
-import com.example.weather.R
+import androidx.fragment.app.FragmentActivity
+import com.example.weather.adapters.VpAdapter
 import com.example.weather.databinding.FragmentMainBinding
 import com.example.weather.isPermissonGranted
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainFragment : Fragment() {
+    private val flist = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
+
+    private val tList = listOf(
+        "HOURS",
+        "DAYS"
+//        R.string.Hours,
+//        R.string.Days
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +40,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    private fun init() = with(binding){
+        val adapter = VpAdapter(activity as FragmentActivity, flist)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout, vp){
+                tab, pos -> tab.text = tList[pos].toString()
+        }.attach()
     }
 
     private fun permissionListener() {
